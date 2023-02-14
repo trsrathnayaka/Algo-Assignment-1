321e72a58754534d459fcd9d273a7e1b1a8f1a11
THEVINDU RANSARA RATHNAYAKA <97075043+trsrathnayaka@users.noreply.github.com>
2023-02-14 12:50:51 +0530

    Quection2

---
+++ Assignment1Q2/Assignment1Q2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//for loop to display all the items of a given array 
+namespace DisplayArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 , 6  };
+            Console.WriteLine("Array elements here:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//.................................T.R.S.Rathnayaka - 23912...................//
